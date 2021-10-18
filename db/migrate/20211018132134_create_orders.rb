27db79d073db464dc7b955b65af391f9f2588530
EC2 Default User <ec2-user@ip-172-31-58-122.ec2.internal>
2021-10-18 14:09:23 +0000

    [Finish]データベース作成

---
+++ db/migrate/20211018132134_create_orders.rb
@@ -0,0 +1,17 @@
+class CreateOrders < ActiveRecord::Migration[5.0]
+  def change
+    create_table :orders do |t|
+      t.integer :id
+      t.integer :customer_id
+      t.string :addresses
+      t.string :name
+      t.integer :payment_method
+      t.integer :postage
+      t.integer :order_active
+      t.integer :billing_amount
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.timestamps
+    end
+  end
+end
---
+++ test/controllers/public_registrations_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PublicRegistrationsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
