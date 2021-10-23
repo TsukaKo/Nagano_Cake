fe8cace30098fb8d7f4c97aa359db5fdc09f3701
TsukaKo <Kodaibb3@gmail.com>
2021-10-23 02:06:47 +0000

    [Add] ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,28 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   devise_for :users
+
+  scope module: :public do
+    resources :items, only:[:index, :show]
+    resources :registrations, only:[:new, :create]
+    resources :sessions, only:[:new, :create, :destroy]
+    resources :customers, only:[:show, :edit, :update]
+    get 'unsubsceibe' => 'customers#unsubsceibe'
+    patch 'withdraw' => 'customers#withdraw'
+    resources :cart_items, only:[:index, :update, :destroy, :create]
+    delete 'all_destroy' => 'cart_items#all_destroy'
+    resources :orders, only:[:new, :create, :index, :show]
+    post 'log' => 'orders#log'
+    get 'thanks' => 'orders#thanks'
+    resources :addresses, only:[:index, :edit, :create, :update, :destroy]
+  end
+
   namespace :admin do
-   resources :sessions, only:[:new, :create, :destroy] 
-   resources :items, only:[:index, :new, :create, :show, :edit, :update]
-   resources :genres, only:[:index, :create, :edit, :update]
-   resources :customers, only:[:index, :show, :edit, :update]
-   resources :orders, only:[:show, :update]
-   resources :orders_details, only:[:update]
-  end 
+    resources :sessions, only:[:new, :create, :destroy]
+    resources :items, only:[:index, :new, :create, :show, :edit, :update]
+    resources :genres, only:[:index, :create, :edit, :update]
+    resources :customers, only:[:index, :show, :edit, :update]
+    resources :orders, only:[:show, :update]
+    resources :orders_details, only:[:update]
+  end
 end
