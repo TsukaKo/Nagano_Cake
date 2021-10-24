91b18586e466530727053fb88100f50e93812e0e
TsukaKo <Kodaibb3@gmail.com>
2021-10-24 06:30:41 +0000

    [Finish] adminログイン画面

--- app/controllers/admin/genres_controller.rb
+++ app/controllers/admin/genres_controller.rb
@@ -2,12 +2,13 @@ class Admin::GenresController < ApplicationController
 
   def index
     @genres = Genre.all
+    @genre = Genre.new
   end
 
   def create
     @genre = Genre.new(genre_params)
     @genre.save
-    redirect_to '/index'
+    redirect_to admin_genres_path
   end
 
   def edit
