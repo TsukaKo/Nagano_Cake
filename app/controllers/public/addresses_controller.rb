69dc9339130cde72b22bd51ce8d87b67593427a8
TsukaKo <Kodaibb3@gmail.com>
2021-10-30 10:59:48 +0000

    [Finish] public/edit

--- app/controllers/public/addresses_controller.rb
+++ app/controllers/public/addresses_controller.rb
@@ -1,2 +1,22 @@
 class Public::AddressesController < ApplicationController
+
+  def create
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:customer_id, :name, :postal_code, :address, :created_at, :update_at)
+  end
 end
