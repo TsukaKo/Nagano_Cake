1778dc02bd76fac006984a5ac6c6a165eab61a1a
TsukaKo <Kodaibb3@gmail.com>
2021-10-31 02:58:01 +0000

    [Add] public/itemsのshowの部分テンプレート

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,12 @@
 class Public::ItemsController < ApplicationController
   
   def index
+    @items = Item.all
+    @genres.all
   end 
   
   def show
+    @item = Item.find(params[:id])
   end 
+  
 end
