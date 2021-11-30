c54f230c03efc8a19f6c578849cf021915d4e40b
TsukaKo <Kodaibb3@gmail.com>
2021-11-30 12:55:53 +0000

    [Start] admin/orderのshow

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,19 @@
 class Admin::OrdersController < ApplicationController
-  
+
   def show
-  end 
-  
+    @order = Order.find(params[:id])
+    @customer = @order.customer
+    #@order_detail = OrderDetail.find(params[:id])
+  end
+
   def update
-  end 
-  
+    @order = Order.find(params[:id])
+    @order.update(orders_params)
+  end
+
+  private
+  def orders_params
+    params.require(:order).permit(:name, :addresses, :payment_method, :postage, :order_active, :billing_amount, :postal_code)
+  end
+
 end
--- app/controllers/admin/orders_details_controller.rb
+++ app/controllers/admin/orders_details_controller.rb
@@ -1,11 +1,13 @@
 class Admin::OrdersDetailsController < ApplicationController
 
   def update
+    @orders_detail = Order.find(params[:id])
+    @orders_detail.update(orders_detail_params)
   end
 
   private
   def orders_detail_params
-    params.require(:order_detail).permit(:amount)
+    params.require(:order_detail).permit(:amount, :maiking_status, :price, :item_id, :order_id)
   end
 
 end
